7f1622105763b33051358f4ee9bfdc16c32839e4
Changehee Choi <changhee.dev@gmail.com>
2020-10-21 22:28:01 +0900

    chore: TypeORM + MySQL 연동

---
+++ ormconfig.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const isProd = process.env.NODE_ENV === 'production';
+const isTest = process.env.NODE_ENV === 'test';
+
+module.exports = {
+    type: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    charset: process.env.DB_CHARSET,
+    dropSchema: isTest ? true : false,
+    synchronize: !isProd ? true : false,
+    logging: !isProd ? true : false,
+    entities: isProd ? ['dist/entity/**/*.js'] : ['src/entity/**/*.ts'],
+};
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,31 @@
 import * as express from 'express';
 import * as logger from 'morgan';
+import { SnakeNamingStrategy } from './config/orm/snake-naming-strategy';
+import { createConnection, getConnectionOptions } from 'typeorm';
 
 const app: express.Application = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const connectDatabase = async () => {
+    try {
+        const connectionOptions = await getConnectionOptions();
+        await createConnection(
+            Object.assign(connectionOptions, {
+                namingStrategy: new SnakeNamingStrategy(),
+            }),
+        );
+        console.log('DB connection success');
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const setMiddlewares = () => {
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+};
+
+connectDatabase();
+setMiddlewares();
 
 export default app;
