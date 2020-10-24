c22a2b15d7888cebf82fb42ab8e1a835016546d4
Changehee Choi <changhee.dev@gmail.com>
2020-10-25 03:01:38 +0900

    chore: webpack 설정 변경
    - 개발용, 배포용 웹팩 설정을 구분하기 위해 공통 설정을 분리하고 webpack-merge 모듈을 사용했었는데 개발 모드 실행방식을 웹팩 빌드 대신 ts-node로 바로 실행하는 방식으로 변경함에 따라 배포용 웹팩 설정만 남김.
    
    - 웹팩 문서를 참고하여 불필요한 설정을 제거하고 일부 설정을 추가하였다.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const nodeExternals = require('webpack-node-externals');
 
@@ -11,20 +12,32 @@ module.exports = {
         filename: 'server.js',
     },
     target: 'node',
+    node: false,
     externals: [nodeExternals()],
     module: {
         rules: [
             {
-                test: /\.(ts|js)?$/,
-                use: {
-                    loader: 'babel-loader',
-                },
+                test: /\.(tsx?|js)$/,
+                exclude: /node_modules/,
+                loader: ['babel-loader'],
             },
         ],
     },
     plugins: [new CleanWebpackPlugin(), new Dotenv()],
     resolve: {
         extensions: ['.ts', '.tsx', '.js'],
+        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
         plugins: [new TsconfigPathsPlugin()],
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    compress: {
+                        drop_console: true,
+                    },
+                },
+            }),
+        ],
     },
 };
